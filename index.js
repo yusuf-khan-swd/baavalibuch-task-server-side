d756a6018ce2ec97e2f8e7c06b48a300aa5854b8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-15 12:47:20 +0600

    post and get end point working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -16,12 +16,32 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     const inputDataCollections = client.db("baavalibuch").collection("inputDataCollections");
-    const inputActionCollections = client.db("baavalibuch").collection("inputActionCollections");
+    const connectionCountCollections = client.db("baavalibuch").collection("connectionCountCollections");
 
     app.post("/posts", async (req, res) => {
       const inputData = req.body;
-      console.log(inputData);
+      const result = await inputDataCollections.insertOne(inputData);
+      res.send(result);
     });
+
+    app.put("/connections", async (req, res) => {
+      console.log("count");
+    });
+
+    app.get("/connections", async (req, res) => {
+      const query = { _id: ObjectId('63c39ee208a5c8c08611a7e6') };
+      const result = await connectionCountCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/connections", async (req, res) => {
+      const data = {
+        count: 1
+      }
+
+      const result = connectionCountCollections.insertOne(data);
+      res.send(result);
+    })
 
   } finally {
 
